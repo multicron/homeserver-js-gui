c9214ab39c46d59449c657daeb7d03d4ba5ebada
Eric Olson <olson@mauicomputing.com>
2021-10-28 10:01:30 -0400

    add core/configuration.js

---
+++ core/configuration.js
@@ -0,0 +1,29 @@
+'use strict';
+
+// import logger from "debug"; const debug = logger('homeserver:configuration');
+
+import config from "../../../src/configuration.js";
+
+export class Configuration {
+
+    static singleton;
+
+    constructor() {
+        if (Configuration.singleton) {
+            return Configuration.singleton;
+        }
+
+        super();
+
+        Object.assign(this, config);
+
+        Configuration.singleton = this;
+    }
+}
+
+const configuration = new Configuration();
+
+export { configuration };
+
+
+
