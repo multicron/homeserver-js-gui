94d8fdad87148321d6a3dc215858d61ffaf51a86
Eric Olson <olson@mauicomputing.com>
2022-10-08 12:56:08 -0400

    MargicGroup -> IconSwitch

--- widget/src/AnyWidget.js
+++ widget/src/AnyWidget.js
@@ -36,7 +36,6 @@ export function AnyWidget(props) {
         case "LightBulb":
         case "Relay":
         case "CompositeDevice":
-        case "MagicGroup":
             return <ToggleSwitch field="power" {...props} />;
             break;
         case "BLEBeacon":
@@ -62,6 +61,7 @@ export function AnyWidget(props) {
         case "FeitElectricBulb":
             return <TasmotaExpandable {...props} />;
             break;
+        case "MagicGroup":
         case "Alarm":
             return <IconSwitch {...props} />;
             break;
